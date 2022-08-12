4928a0943b6671ef456a4ee75fc121b751d92add
zzcand111 <31539914+zzcand111@users.noreply.github.com>
2022-08-13 07:03:35 +0800

    Create diy.sh

--- diy.sh
+++ diy.sh
@@ -16,8 +16,7 @@ sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 svn co https://github.com/zzcand/luci-app-easymesh package/luci-app-easymesh
 
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git easymesh https://github.com/zzcand/luci-app-easymesh' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.defaut
 
 # Update feeds
 ./scripts/feeds update -a
